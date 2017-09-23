8955600c758884a73c4250b56de146775031d26a
Carl-June Piao <cgpiao1@up.edu.ph>
2017-09-22 22:26:21 -0700

    Completed Unit Testing

--- spotify.js
+++ spotify.js
@@ -1,77 +1,132 @@
 function Spotify()
 {
-	console.log("Spotify here");
 	var list_of_singers = [];
 	this.length = 0;
 
-
-//the singer entity
-var singer = function singer (name, description,){
+	/*-------------------ENTITIES----------------------------*/
+	//Singer Entity
+	var singer = function(name, description,){
 		var arr = []
-		return { name:name, description:description,albums:arr};
+		return { 
+			name:name, 
+			description:description,
+			albums:arr
+		}
 	}
-//the song entity
-var song = function song(title, length){
-		this.title = title;
+
+	//Album Entity	
+	var album = function(title,desciption)
+	{
+		this.song_count = 0;
+		arr = [];
+		return {
+			title:title,
+			songs:arr
+		}	
 	}
-//the album entity	
-var album = function(title,desciption)
-{
-			this.song_count = 0;
-			arr = [];
-			return {title:title,songs:arr};		
-}
 
-this.addSong = function (song_title, album_title, singer){
+	//Song Entity
+	var song = function(title)
+	{
+		return {
+			title:title
+		}
+	}
 
-    	list_of_singers[getSinger(siger)].album[findAlbum()].songs[this.song_count] = song_title;
-    	list_of_singers[getSinger(siger)].album[findAlbum()].this.song_count += 1;
-};
-this.addSinger = function (name, description){
-		console.log("Spotify here 1");
-		kantaher = new singer(name,description);
-		list_of_singers[this.length] = kantaher;
+	/*-------------------ADDING METHODS----------------------*/	
+	//Add Singer
+	this.addSinger = function (name, description){
+		var artist = new singer(name,description);
+		list_of_singers[this.length] = artist;
 		this.length += 1;
-	    console.log(kantaher.name + "name" + this.length + "count");
-		
+	};
 
-};
-this.AddAlbum = function (singer,album_title,album_description){
+	//Add Album
+	this.addAlbum = function (singer,album_title,album_description){
 		var alb = new album(album_title,album_description);
 		alb_len = list_of_singers[this.getSinger(singer)].albums.length;
 		list_of_singers[this.getSinger(singer)].albums[alb_len] = alb;
 		list_of_singers[this.getSinger(singer)].albums.length += 1;
+	};
+	
+	//Add Song
+	this.addSong = function (song_title, album_title, singer){
+		var s = new song(song_title);
+		songs_len = list_of_singers[this.getSinger(singer)].albums[this.getAlbum(singer,album_title)].songs.length;
+		list_of_singers[this.getSinger(singer)].albums[this.getAlbum(singer,album_title)].songs[songs_len] = s;
+		list_of_singers[this.getSinger(singer)].albums[this.getAlbum(singer,album_title)].songs.length += 1;
+	};
 
-
-};
-this.getAlbum = function(singer, album_name){
-	   artist = list_of_singers[getSinger(singer)];
-	   for (var i = 0; i <= artist.this.album_count; i++) {
-	   			if(artist.albums[i] == album_name){
-	   				return i;
-	   			}
-	   };
-	   return -1;
-}
-this.getSinger = function(singer){
-	//finds the singer in the array and returns the singer;
-	for (var i  = 0; i < list_of_singers.length; i++) {
-		
-		if(list_of_singers[i].name == singer){
-			console.log("Singer::" + list_of_singers[i].name);
-			return i;
+	/*----------------------GET METHODS----------------------*/	
+	//Get Singer
+	this.getSinger = function(singer){
+		//finds the singer in the array and returns the singer;
+		for (var i  = 0; i < list_of_singers.length; i++) {
+			if(list_of_singers[i].name == singer){
+				return i;
+			}
 		}
-	};
-	return -1;
-}
-this.displayAllAlbums = function(singer){
-	albumss = list_of_singers[this.getSinger(singer)].albums;
-	arr = [];
-	albumss.length -= 1;
-	for (var i = 0; i < albumss.length; i++) {
-			console.log(i + ":: i");
-			arr[i] = list_of_singers[this.getSinger(singer)].albums[i].title;
-	};
-	return arr;
-}
+		return -1;
+	}
+	
+	//Get Album
+	this.getAlbum = function(singer, album_name){
+		artist = list_of_singers[this.getSinger(singer)];
+
+		for (var i = 0; i <= artist.albums.length; i++) {
+			if(artist.albums[i].title == album_name){
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	//Get Song
+	this.getSong = function(song_title, album_name, singer){
+		artist = list_of_singers[this.getSinger(singer)];
+
+		//Loop inside the singer's album
+		for (var i = 0; i <= artist.albums.length; i++) {
+
+			//Check if album we are looking for is in the singer's album
+			if(artist.albums[i] == album_name){
+
+				//Find the song we are looking inside the album
+				for(var j = 0; j < artist.albums[i].songs.length;j++){
+					if(artist.albums[i].songs[i].title == song_title){
+						return j; //return the song
+					}
+				}
+			}
+		}
+		return -1; //if not existing
+	}
+	
+	/*------------------------DISPLAY METHODS----------------------*/		
+	//Display All Albums
+	this.displayAllAlbums = function(singer){
+		albumss = list_of_singers[this.getSinger(singer)].albums;
+		arr = [];
+		albumss.length -= 1;
+
+		for (var i = 0; i < albumss.length; i++) {
+				arr[i] = list_of_singers[this.getSinger(singer)].albums[i].title;
+		}
+
+		return arr;
+	}
+
+	//Display All Songs
+	this.displayAllSongs = function(singer,album){
+		list_songs = list_of_singers[this.getSinger(singer)].albums[this.getAlbum(singer,album)].songs;
+		arr = [];
+
+		list_songs.length -= 1;
+
+		for (var i = 0; i < list_songs.length; i++) {
+			arr[i] = list_of_singers[this.getSinger(singer)].albums[i].songs[i].title;
+		}
+
+		return arr;
+	}
 }
--- tests.js
+++ tests.js
@@ -3,50 +3,45 @@ chai.config.truncateThreshold = 0;
 var assert = chai.assert;
 
 describe('Spotify', function(){
-    describe('addSinger(value,value)', function(){
+    describe('Add_Singer(value,value)', function(){
         beforeEach(function(){
             this.spotify = new Spotify();
         });
         it('Should add singers to the list of singers', function(){
-
             this.spotify.addSinger('Lany','Mga lami nga lalaki');
             assert.equal(this.spotify.length, 1);
-
             this.spotify.addSinger('Missing Filemon','Daddehs');
             assert.equal(this.spotify.length, 2);
-
             this.spotify.addSinger('Taylor Swift','PLAYING VICTIM');
             assert.equal(this.spotify.length, 3);
         });
-        });
-    
-        describe('AddAlbum(value,value,value)', function(){
-            beforeEach(function(){
-                this.spotify = new Spotify();
-                  this.spotify.addSinger('Lany','Mga lami nga lalaki');
-                  this.spotify.addSinger('Kodaline','Mga maoy na lalaki');
-            });
-            it('Should add an album to the singer', function(){
+    });
 
-                this.spotify.AddAlbum('Kodaline','In a perfect world', 'Sad album');
-                assert.deepEqual(this.spotify.displayAllAlbums('Kodaline'), ['In a perfect world']);
-                this.spotify.AddAlbum('Kodaline','Random Album', 'Random Album');
-                assert.deepEqual(this.spotify.displayAllAlbums('Kodaline'), ['In a perfect world','Random Album']);
-            });
+    describe('Add_Album(value,value,value)', function(){
+        beforeEach(function(){
+            this.spotify = new Spotify();
+            this.spotify.addSinger('Lany','Mga lami nga lalaki');
+            this.spotify.addSinger('Kodaline','Mga maoy na lalaki');
         });
-        describe('AddAlbum(value,value,value)', function(){
-            beforeEach(function(){
-                this.spotify = new Spotify();
-                  this.spotify.addSinger('Lany','Mga lami nga lalaki');
-                  this.spotify.addSinger('Kodaline','Mga maoy na lalaki');
-            });
-            it('Should add an album to the singer', function(){
+        it('Should add an album to the singer', function(){
+            this.spotify.addAlbum('Kodaline','In a perfect world', 'Sad album');
+            assert.deepEqual(this.spotify.displayAllAlbums('Kodaline'), ['In a perfect world']);
+            this.spotify.addAlbum('Kodaline','Random Album', 'Random Album');
+            assert.deepEqual(this.spotify.displayAllAlbums('Kodaline'), ['In a perfect world','Random Album']);
+            this.spotify.addAlbum('Kodaline','New Album', 'Random Album');
+            assert.deepEqual(this.spotify.displayAllAlbums('Kodaline'), ['In a perfect world','Random Album','New Album']);
+        });
+    });
 
-                this.spotify.AddAlbum('Kodaline','In a perfect world', 'Sad album');
-                assert.deepEqual(this.spotify.displayAllAlbums('Kodaline'), ['In a perfect world']);
-                this.spotify.AddAlbum('Kodaline','Random Album', 'Random Album');
-                assert.deepEqual(this.spotify.displayAllAlbums('Kodaline'), ['In a perfect world','Random Album']);
-            });
+    describe('Add_Song(value,value,value)', function(){
+        beforeEach(function(){
+            this.spotify = new Spotify();
+            this.spotify.addSinger('Chigga','Black Indian Chinese');
+            this.spotify.addAlbum('Chigga','Album Chigga', 'Race Descipriton Album');
         });
+        it('Should add a song in the album', function(){
+            this.spotify.addSong('Song1','Album Chigga','Chigga');
+            assert.deepEqual(this.spotify.displayAllSongs('Chigga','Album Chigga'), ['Song1']);
+        });
+    });
 });
-
