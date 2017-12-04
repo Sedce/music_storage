4f440e8f8bcf4e184a97123a42c799262ef3c18d
sedce <cggulane@up.edu.ph>
2017-12-04 10:49:36 +0800

    PUSH

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const connectionUrl = 'postgres://carlpiao:carlpiao@localhost:5432/spotyfi';
-const database = new Sequelize(connectionUrl);
+const connectionUrl = 'postgres://carlpiao:carlpiao@localhost:5432/spotyfi' ;
+const database = new Sequelize(connectionUrl) || process.env.DATABASE_URL;
 
 module.exports = database;
