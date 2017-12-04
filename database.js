afb11ce074688d061fb2b5a968080386201f31bf
Cedez Marie G. Gulane <cggulane@up.edu.ph>
2017-12-04 11:24:16 +0800

    Update database.js

--- database.js
+++ database.js
@@ -1,5 +1,16 @@
 const Sequelize = require('sequelize');
 const connectionUrl = 'postgres://carlpiao:carlpiao@localhost:5432/spotyfi' ;
-const database = new Sequelize(connectionUrl) || process.env.DATABASE_URL;
+const database;
+if(process.env.DATABASE_URL){
+  database = new Sequelize(process.env.DATABASE_URL,{
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: true
+  });
+
+
+}else{
+  var database = new Sequelize(connectionUrl)
+}
 
 module.exports = database;
